019a79b5d96cc76c763c6dec581d8042946fe667
nurullah91 <mdnurullah.ptk@gmail.com>
2023-04-07 01:41:30 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/orders">Orders</Link>
+            <Link to="home">Home</Link>
+            <Link to="home">Home</Link>
+
+
+        </nav>
+    );
+};
+
+export default Header;
